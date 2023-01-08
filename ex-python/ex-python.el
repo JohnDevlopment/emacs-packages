fc6bbe7c435b0a053cf897ac3253882890dc224e
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2023-01-07 21:18:18 -0600

    ex-python: Fix docstrings
    Change 'ex-python-lsp-enabled' to 'ex-python-use-lsp'

--- ex-python/ex-python.el
+++ ex-python/ex-python.el
@@ -11,8 +11,12 @@
 ;;; Commentary:
 ;;
 ;; Extra functions that are not available in the default Python
-;; mode implementation. These are functions that I wanted for
-;; myself
+;; mode implementation.
+;;
+;; Commands:
+;;
+;;     `python-add-docstring': adds a docstring to the current
+;;     function.
 
 ;;; Code:
 
@@ -22,15 +26,21 @@
   "Extra functionality for Python mode."
   :group 'python)
 
-(defcustom ex-python-lsp-enabled nil
-  "Non-nil means that lsp is enabled.
-Enables lsp-related functions."
+(defcustom ex-python-use-lsp nil
+  "Non-nil means to use lsp functions."
   :type 'boolean
   :safe 'booleanp)
 
 ;;;###autoload
 (defun python-add-docstring (initval yn)
-  "Add a docstring to the function."
+  "Add a docstring to the function.
+
+If called interactively, prompts for the
+initial line of the docstring, then asks
+whether the docstring should be multiline.
+
+If called from Lisp, INITVAL provides the first line,
+and YN Is a character that is either 'y' or 'n'."
   (interactive "MFirst line: \ncmultiline? [y/n]")
   (let ((multiline (string yn)) (delim "\"\"\"")
 	(standard-output (current-buffer)))
@@ -67,6 +77,7 @@ Enables lsp-related functions."
 	"--"
 	("LSP"
 	 :active (ex-python--lsp-enabled-p)
+	 :help "A submenu for lsp commands."
 	 ["Find definition" lsp-find-definition])
 	"---"
 	["Disable Ex-Python mode" ex-python-mode]))
@@ -78,9 +89,11 @@ Enables lsp-related functions."
   "This minor mode should only be used while in Python mode. It
 adds extra commands that are not provided in Python mode.
 
-If `ex-python-lsp-enabled' is set to a non-nill value, and `lsp'
+If `ex-python-use-lsp' is set to a non-nill value, and `lsp'
 is installed and Emacs configured to use it, the Ex-Python menu
-contains a submenu dedicated to lsp commands."
+contains a submenu dedicated to lsp commands.
+
+\\{ex-python-mode-map}"
 
   :lighter " xpy"
   :interactive '(python-mode)
@@ -91,8 +104,9 @@ contains a submenu dedicated to lsp commands."
 ;; Internal functions
 
 (defun ex-python--lsp-enabled-p ()
+  "Return non-nill if we can use lsp functions."
   (and
-   ex-python-lsp-enabled
+   ex-python-use-lsp
    (fboundp 'lsp)))
 
 (provide 'ex-python)
