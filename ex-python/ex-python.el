121130839a9d6aa1b292e27c9a1ed25a4573eb92
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2023-03-26 20:36:06 -0500

    ex-python: Bump to version 1.0-alpha6

--- ex-python/ex-python.el
+++ ex-python/ex-python.el
@@ -3,8 +3,8 @@
 ;; Copyright (C) 2023 John Russell
 ;; Author: John Russell <johndevlopment7@gmail.com>
 ;; Keywords: languages
-;; Package-Version: 1.0-alpha5
-;; Version: 1.0-alpha5
+;; Package-Version: 1.0-alpha6
+;; Version: 1.0-alpha6
 ;;
 ;;; This file is NOT part of GNU Emacs
 
